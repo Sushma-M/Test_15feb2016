ef4c248027977fd32104d46c3852be7b17de45d4
Sushma Wavemaker null <sushma.maddirala@wavemaker.com>
2016-02-15 07:00:52 +0000

    New files added - [] Files modified - [wmProperties.js] Refactored files - [] Fixed bugs - [] Removed files - []

--- src/main/webapp/wmProperties.js
+++ src/main/webapp/wmProperties.js
@@ -1,1 +1,1 @@
-var _WM_APP_PROPERTIES = {"name":"dbTest1212","version":"1.0","defaultLanguage":"en","type":"APPLICATION","homePage":"Main","platformType":"WEB"}
+var _WM_APP_PROPERTIES = {"name":"dbTest1212","version":"1.0","defaultLanguage":"en","type":"APPLICATION","homePage":"Main","platformType":"WEB","activeTheme":"default"}
